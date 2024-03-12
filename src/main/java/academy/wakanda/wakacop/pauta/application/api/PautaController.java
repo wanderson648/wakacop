926289a22d57e4c973b8ea6695b419908a9cfc34
wanderson648 <testepedroca@gmail.com>
2024-03-11 23:22:42 -0300

    fix api response e controller

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaController.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaController.java
@@ -3,13 +3,11 @@ package academy.wakanda.wakacop.pauta.application.api;
 import academy.wakanda.wakacop.pauta.application.service.PautaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
-@RequestMapping("/pauta")
 public class PautaController implements PautaAPI {
 
     private final PautaService pautaService;
